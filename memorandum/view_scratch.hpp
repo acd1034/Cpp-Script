784409af04b51bff180addca12b4f7556ad1e100
acd1034 <48613285+acd1034@users.noreply.github.com>
2019-12-06 19:38:27 +0900

    tasks.json

--- memorandum/view_scratch.hpp
+++ memorandum/view_scratch.hpp
@@ -13,7 +13,7 @@ template <typename urng_t>
 class view_add_constant
 // : public ranges::view_base
 {
-  private:
+private:
   /* data members == "the state" */
   struct data_members_t {
     urng_t urange;
@@ -29,7 +29,6 @@ class view_add_constant
     iterator_type(base const& b) : base{b} {}
 
     iterator_type operator++(int) { return static_cast<base&>(*this)++; }
-
     iterator_type& operator++() {
       ++static_cast<base&>(*this);
       return (*this);
@@ -38,33 +37,30 @@ class view_add_constant
     reference operator*() const { return *static_cast<base>(*this) + 42; }
   };
 
-  public:
+public:
   /* member type definitions */
   using reference       = uint64_t;
   using const_reference = uint64_t;
   using value_type      = uint64_t;
-
-  using iterator       = iterator_type;
-  using const_iterator = iterator_type;
+  using iterator        = iterator_type;
+  using const_iterator  = iterator_type;
 
   /* constructors and deconstructors */
   view_add_constant()                                       = default;
   constexpr view_add_constant(view_add_constant const& rhs) = default;
   constexpr view_add_constant(view_add_constant&& rhs)      = default;
-  constexpr view_add_constant& operator=(view_add_constant const& rhs) = default;
+  constexpr view_add_constant&
+  operator=(view_add_constant const& rhs) = default;
   constexpr view_add_constant& operator=(view_add_constant&& rhs) = default;
   ~view_add_constant()                                            = default;
-
   view_add_constant(urng_t&& urange)
     : data_members{new data_members_t{std::forward<urng_t>(urange)}} {}
 
   /* begin and end */
   iterator begin() const { return std::begin(data_members->urange); }
-  iterator cbegin() const { return begin(); }
-
-  auto end() const { return std::end(data_members->urange); }
-
-  auto cend() const { return end(); }
+  const_iterator cbegin() const { return begin(); }
+  iterator end() const { return std::end(data_members->urange); }
+  const_iterator cend() const { return end(); }
 };
 
 template <typename urng_t>
@@ -92,13 +88,11 @@ struct add_constant_fn {
 };
 
 namespace view {
-
   add_constant_fn constexpr add_constant;
 }
 
 int main() {
   std::vector<uint64_t> in{1, 4, 6, 89, 56, 45, 7};
-
   for (auto&& i : in | view::add_constant) std::cout << i << ' ';
   std::cout << '\n'; // should print: 43 47 64 131 98 87 49
 }
