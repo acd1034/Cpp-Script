194d09ea3541e74f3db4725d28a49b89e0f3759e
acd1034 <48613285+acd1034@users.noreply.github.com>
2020-03-13 17:48:03 +0900

    draft

--- draft/Untitled.cpp
+++ draft/Untitled.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+template <class CharT, class Traits, class R>
+auto& operator<<(std::basic_ostream<CharT, Traits>& os, const R& r) {
+  const char* dlm = "";
+  for (const auto& x : r) os << std::exchange(dlm, " ") << x;
+  return os;
+}
 int main() {
   using namespace std::literals;
   std::cout << "Hello world!"sv << std::endl;
