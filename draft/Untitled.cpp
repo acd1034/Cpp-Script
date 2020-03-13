4361983257d7d7467e0546f74dc65d1e4aa83dd7
acd1034 <48613285+acd1034@users.noreply.github.com>
2020-03-13 17:33:51 +0900

    update

---
+++ draft/Untitled.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  using namespace std::literals;
+  std::cout << "Hello world!"sv << std::endl;
+}
--- include/bits/stdc++.h
+++ include/bits/stdc++.h
@@ -1,15 +1,85 @@
-// C++ includes
-#include <iostream>
-#include <iomanip>
+// C++ includes used for precompiling -*- C++ -*-
+// https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/include/precompiled/stdc%2B%2B.h
+
 #include <algorithm>
-#include <cmath>
-#include <complex>
-#include <numeric>
-#include <array>
 #include <bitset>
+#include <complex>
 #include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
 #include <map>
 #include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
 #include <queue>
 #include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
 #include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+// #include <atomic>
+#include <chrono>
+// #include <codecvt> // deprecated in C++17
+// #include <condition_variable>
+#include <forward_list>
+// #include <future>
+#include <initializer_list>
+// #include <mutex>
+#include <random>
+#include <ratio>
+// #include <regex>
+// #include <scoped_allocator>
+// #include <system_error>
+// #include <thread>
+#include <tuple>
+#include <type_traits>
+// #include <typeindex>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+// #if __cplusplus >= 201402L
+// #include <shared_mutex>
+// #endif
+
+#if __cplusplus >= 201703L
+#include <any>
+// #include <charconv>
+// #include <execution>
+// #include <filesystem>
+// #include <memory_resource>
+#include <optional>
+#include <string_view>
+#include <variant>
+#endif
+
+#if __cplusplus > 201703L
+#include <bit>
+#include <compare>
+// #include <concepts>
+// #include <numbers>
+// #include <ranges>
+#include <span>
+// #include <stop_token>
+// #include <syncstream>
+#include <version>
+#endif
