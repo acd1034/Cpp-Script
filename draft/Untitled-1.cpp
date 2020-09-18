28fd6135868f4d3f393dff8c85ae49ed2256e307
acd1034 <48613285+acd1034@users.noreply.github.com>
2020-09-18 16:44:37 +0900

    fix

--- draft/Untitled-1.cpp
+++ draft/Untitled-1.cpp
@@ -1,10 +1,17 @@
+#include <cstddef> // size_t, ptrdiff_t
+#include <cstdint> // int32_t
+#include <utility> // initializer_list, move, forward, pair
+
 #include <iostream>
+#include <string_view>
+
 template <class CharT, class Traits, class R>
 auto& operator<<(std::basic_ostream<CharT, Traits>& os, const R& r) {
-  const char* dlm = "";
+  std::string_view dlm{""};
   for (const auto& x : r) os << std::exchange(dlm, " ") << x;
   return os;
 }
+
 int main() {
   using namespace std::literals;
   std::cout << "Hello world!"sv << std::endl;
