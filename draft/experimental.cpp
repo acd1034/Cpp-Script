871a1acd67d2a587e0a9ca1d47b95c6d960b8f19
acd1034 <48613285+acd1034@users.noreply.github.com>
2021-03-27 23:18:54 +0900

    add draft

---
+++ draft/experimental.cpp
@@ -0,0 +1,32 @@
+namespace ns {
+  struct identity {
+    template <typename T>
+    constexpr T&& operator()(T&& t) const noexcept(noexcept(std::forward<T>(t))) {
+      return std::forward<T>(t);
+    }
+  };
+
+  // https://en.cppreference.com/w/cpp/utility/variant/visit
+  template <typename... Ts>
+  struct overloaded : Ts... {
+    using Ts::operator()...;
+  };
+  template <typename... Ts>
+  overloaded(Ts...) -> overloaded<Ts...>;
+
+  template <typename Fn>
+  struct scope_exit {
+  private:
+    Fn fn;
+
+  public:
+    constexpr explicit scope_exit(Fn const& fn) noexcept : fn(fn) {}
+    constexpr explicit scope_exit(Fn&& fn) noexcept : fn(std::move(fn)) {}
+    scope_exit() = delete;
+    scope_exit(scope_exit const&) = delete;
+    scope_exit(scope_exit&&) = delete;
+    void operator=(scope_exit const&) = delete;
+    void operator=(scope_exit&&) = delete;
+    ~scope_exit() noexcept { fn(); }
+  };
+} // namespace ns
