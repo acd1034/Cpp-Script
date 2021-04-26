f5240e339028fd49d64ac97947e70476af984f0a
acd1034 <48613285+acd1034@users.noreply.github.com>
2021-04-27 01:13:13 +0900

    fix include

--- include/ns/core.hpp
+++ include/ns/core.hpp
@@ -4,7 +4,7 @@
 #include <cstddef>          // size_t, ptrdiff_t
 #include <cstdint>          // int32_t
 #include <initializer_list> // initializer_list
-#include <iostream>
+#include <iosfwd>
 #include <tuple> // tuple
 #include <type_traits>
 #include <utility>      // move, forward, pair
